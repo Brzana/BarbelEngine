c3578258afb1f085121876a2eb69f8550f4a64c2
Brzana <szymonbrzana@gmail.com>
2026-01-17 16:37:34 +0100

    feat: getting rid of magic numbers in Main.cpp

--- Sandbox/src/Main.cpp
+++ Sandbox/src/Main.cpp
@@ -1,8 +1,12 @@
 #include <Engine.h>
 
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+const char* WINDOW_TITLE = "Sandbox";
+
 int main() {
 
-	Engine::Engine engine(800, 600, "Sandbox");
+	Engine::Engine engine(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
 
 	engine.Run();
 
